b150b2ffeb4cdf3de0e271a69e7bfd08ce30d51e
Brian Poole <brianpooledev@gmail.com>
2019-07-21 13:32:58 -0500

    Added custom hook for local storage

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -9,14 +9,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
 const TodoApp = () => {
-  const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]');
+  const initialTodos = [{ id: 1, task: 'Pet a Monkey', completed: false }];
   const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
     initialTodos
   );
-
-  useEffect(() => {
-    window.localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
 
   return (
     <Paper
---
+++ src/hooks/useLocalStorageState.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function UseLocalStorageState(key, defaultVal) {
+  // Make piece of state based off of value in localstorage (or default)
+  const [state, setState] = useState(() => {
+    let val;
+    try {
+      val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    } catch (e) {
+      val = defaultVal;
+    }
+    return val;
+  });
+
+  // use useEffect to update localstorage when state changes
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state]);
+  return [state, setState];
+}
+
+export default UseLocalStorageState;
